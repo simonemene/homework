ee8188841e54539b5d233834c1175a22b5bdb87c
simonemene <37829230+simonemene@users.noreply.github.com>
2018-04-15 22:19:43 +0200

    Add files via upload

---
+++ src/node1.cpp
@@ -0,0 +1,78 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+std::string comando = "";
+
+
+
+
+
+void chatterCallback(const std_msgs::String::ConstPtr& msg){
+
+	comando = msg->data;
+	
+	
+
+}
+
+
+
+int main(int argc, char **argv) {
+
+
+	ros::init(argc,argv,"nodo1");
+	
+	ros::NodeHandle ns;
+
+
+	ros::Publisher nodo = ns.advertise<std_msgs::String>("dati",1000);
+
+	ros::Subscriber sub = ns.subscribe("comando",1000,chatterCallback);
+
+	
+	ros::Rate loop_rate(1);
+
+	while( ros::ok() ) {
+		
+
+		std_msgs::String msg;
+		std::stringstream nome;
+		nome << "Marco_Rossi 23 Informatica";
+		msg.data = nome.str();
+
+		nodo.publish(msg);
+
+		loop_rate.sleep();
+		ros::spinOnce();
+
+		ros::spinOnce();
+		
+		if(comando == "u"){
+			ros::shutdown();
+			
+		}
+
+		
+
+		
+		
+		
+	}
+
+	
+
+	
+
+	
+
+	
+
+	return 0;
+}
+		
+		
+
+	
+
+	
